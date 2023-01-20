78dd87190d6cfd35b6401ade8050f438ab768555
Giovanni Menzano <giovanni.menzano@gmail.com>
2023-01-20 02:55:36 +0100

    completed function to change board dimension; other minor changes.

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -1,36 +1,50 @@
-/* 
-get the grid, requested solution and solve button to start the rest call,
-previous and next button to cycle through possible solutions,
-and the other necessary elements
-*/
-
-const gridElement = document.querySelector(".grid");
+const boardDimensionElement = document.querySelector("#board-dimension");
 const requestedSolutionsElement = document.querySelector("#requested-solutions");
-const solveButtonElement = document.querySelector("#solve-button");
-const resetButtonElement = document.querySelector("#reset-button");
 const currentSolutionElement = document.querySelector("#current-solution");
 const solutionsFoundElement = document.querySelector("#solutions-found");
+
+const gridElement = document.querySelector(".grid");
+const solveButtonElement = document.querySelector("#solve-button");
+const resetButtonElement = document.querySelector("#reset-button");
 const previousButtonElement = document.querySelector("#previous-solution");
 const nextButtonElement = document.querySelector("#next-solution");
 
+const cornerCellHTML = '<div class="cell cell--corner"></div>';
+const frameCellHTML = '<div class="cell cell--frame"><input type="number" value="0"></div>';
+const boardCellHTML = '<div class="cell cell--input"><input type="number" value="0"></div>';
+
 const host = "localhost:8090";
 
-let gridDimension = Math.sqrt(gridElement.querySelectorAll(".cell").length);
 let solutions = [];
 let currentSolutionIndex = 0;
 
-// removed form to properly validate fields without default HTML validation
-/*
-document.querySelector("#solver-form").addEventListener("submit", function(event) {
+boardDimensionElement.addEventListener("change", () => {
+	debugger;
+	let newGrid = '';
+	const dimension = parseInt(boardDimensionElement.value) + 2;
 
-	console.log("form submit event received");
-	
-	// the submit event is fired, this means I'm making the call to the solver service, so I disable the submit button and enable it again when the response is received
-	document.getElementById("submit-button").setAttribute("disabled", true);
-	event.preventDefault(); // allow customized form submit via event listener on solve button
+	// create the new grid HTML
+	for (let i = 0; i < dimension; i++) {debugger;
+		for (let j = 0; j < dimension; j++) {
 
-});
-*/
+			if ((i === 0 && j === 0) || (i === 0 && j === dimension - 1) || (i === dimension - 1 && j === 0) || (i === dimension - 1 && j === dimension - 1)) {
+				newGrid += cornerCellHTML;
+			} else if (i === 0 || i === dimension - 1 || j === 0 || j === dimension - 1) {
+				newGrid += frameCellHTML;
+			} else {
+				newGrid += boardCellHTML;
+			}
+			
+		}
+	}
+
+	// update the grid's innerHTML with the new grid
+	gridElement.innerHTML = newGrid;
+	// change the grid css for columns and rows, this will be inline, so it will have priority over the css file
+	gridElement.style["grid-template-columns"] = "repeat(" + dimension + ", 1fr)";
+	gridElement.style["grid-template-rows"] = "repeat(" + dimension + ", 1fr)";
+
+}, false);
 
 // event listener for cell inputs
 gridElement.addEventListener("input", (e) => {
@@ -38,9 +52,9 @@ gridElement.addEventListener("input", (e) => {
 	// listener is added to the grid, but the target are the input cells
     if (e.target.matches("input")) {
 
-		// the max number the user can type is the grid dimension minus two, corresponding to the game table dimension
-		const maxInput = gridDimension - 2;
-        // delete every character not matching a number between 1 and the max number
+		// the max number the user can type is the grid dimension minus two, corresponding to the game board dimension
+		const maxInput = parseInt(boardDimensionElement.value);
+        // delete every character not matching a number between 1 and the max number for the selected board
 		e.target.value = e.target.value.replace(new RegExp("[^1-" + maxInput + "]", "g"),"");
 
 		// force user to insert only one character
@@ -57,8 +71,8 @@ solveButtonElement.addEventListener("click", async () => {
 
 	/*
 	convert grid to array
-	if cell has no input (angle cell) or cell input value is null, return 0
-	*/debugger;
+	if cell has no input (corner cell) or cell input value is null, return 0
+	*/
 	// const inputBoard = Array.from(gridElement.querySelectorAll(".cell"), cell => {
 	// 	if (cell.input && cell.input.value) {
 	// 		return cell.input.value;
@@ -72,9 +86,9 @@ solveButtonElement.addEventListener("click", async () => {
 
 	const inputBoard = [];
 	while (grid.length) {
-		inputBoard.push(grid.splice(0, gridDimension));
+		inputBoard.push(grid.splice(0, parseInt(boardDimensionElement.value) + 2));
 	}
-	const requestJson = JSON.stringify({ inputBoard: inputBoard })
+	const requestJson = JSON.stringify({ inputBoard: inputBoard }); debugger;
 	// get requested solutions number
 	const requestedSolutions = requestedSolutionsElement.value;
 
