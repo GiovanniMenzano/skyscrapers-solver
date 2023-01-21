0d08dfe15f1c5469bc5b592b659675468d32fcb3
Giovanni Menzano <giovanni.menzano@gmail.com>
2023-01-21 01:46:11 +0100

    forgot to add this one to the last commit :).

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -4,14 +4,22 @@ const currentSolutionElement = document.querySelector("#current-solution");
 const solutionsFoundElement = document.querySelector("#solutions-found");
 
 const gridElement = document.querySelector(".grid");
-const solveButtonElement = document.querySelector("#solve-button");
-const resetButtonElement = document.querySelector("#reset-button");
 const previousButtonElement = document.querySelector("#previous-solution");
 const nextButtonElement = document.querySelector("#next-solution");
+const solveButtonElement = document.querySelector("#solve-button");
+const resetButtonElement = document.querySelector("#reset-button");
+const exampleButtonElement = document.querySelector("#example-button");
 
-const cornerCellHTML = '<div class="cell cell--corner"></div>';
+const cornerCellHTML = '<div class="cell cell--corner"><input type="number" value="0"></div>';
 const frameCellHTML = '<div class="cell cell--frame"><input type="number" value="0"></div>';
-const boardCellHTML = '<div class="cell cell--input"><input type="number" value="0"></div>';
+const boardCellHTML = '<div class="cell cell--board"><input type="number" value="0"></div>';
+const exampleValues =	[[0, 4, 2, 1, 2, 3, 0],
+						[3, 0, 0, 0, 0, 0, 3],
+						[2, 0, 0, 0, 0, 0, 4],
+						[3, 0, 0, 0, 0, 0, 1],
+						[2, 0, 0, 0, 0, 0, 2],
+						[1, 0, 0, 0, 0, 0, 2],
+						[0, 1, 4, 3, 2, 2, 0]];
 
 const host = "localhost:8090";
 
@@ -19,32 +27,12 @@ let solutions = [];
 let currentSolutionIndex = 0;
 
 boardDimensionElement.addEventListener("change", () => {
-	debugger;
-	let newGrid = '';
+
+	resetSolutions();
 	const dimension = parseInt(boardDimensionElement.value) + 2;
+	changeGridDimension(dimension);
 
-	// create the new grid HTML
-	for (let i = 0; i < dimension; i++) {debugger;
-		for (let j = 0; j < dimension; j++) {
-
-			if ((i === 0 && j === 0) || (i === 0 && j === dimension - 1) || (i === dimension - 1 && j === 0) || (i === dimension - 1 && j === dimension - 1)) {
-				newGrid += cornerCellHTML;
-			} else if (i === 0 || i === dimension - 1 || j === 0 || j === dimension - 1) {
-				newGrid += frameCellHTML;
-			} else {
-				newGrid += boardCellHTML;
-			}
-			
-		}
-	}
-
-	// update the grid's innerHTML with the new grid
-	gridElement.innerHTML = newGrid;
-	// change the grid css for columns and rows, this will be inline, so it will have priority over the css file
-	gridElement.style["grid-template-columns"] = "repeat(" + dimension + ", 1fr)";
-	gridElement.style["grid-template-rows"] = "repeat(" + dimension + ", 1fr)";
-
-}, false);
+});
 
 // event listener for cell inputs
 gridElement.addEventListener("input", (e) => {
@@ -66,6 +54,26 @@ gridElement.addEventListener("input", (e) => {
 
 }, true);
 
+previousButtonElement.addEventListener("click", () => {
+
+	if(solutions.length > 0) {
+		currentSolutionIndex = currentSolutionIndex > 0 ? currentSolutionIndex - 1 : solutions.length - 1;
+		currentSolutionElement.innerHTML = currentSolutionIndex + 1;
+		fillGrid(solutions[currentSolutionIndex]);
+	}
+
+});
+
+nextButtonElement.addEventListener("click", () => {
+
+	if(solutions.length > 0) {
+		currentSolutionIndex = currentSolutionIndex < solutions.length - 1 ? currentSolutionIndex + 1 : 0;
+		currentSolutionElement.innerHTML = currentSolutionIndex + 1;
+		fillGrid(solutions[currentSolutionIndex]);
+	}
+
+});
+
 // event listener for solve button
 solveButtonElement.addEventListener("click", async () => {
 
@@ -73,13 +81,13 @@ solveButtonElement.addEventListener("click", async () => {
 	convert grid to array
 	if cell has no input (corner cell) or cell input value is null, return 0
 	*/
-	// const inputBoard = Array.from(gridElement.querySelectorAll(".cell"), cell => {
-	// 	if (cell.input && cell.input.value) {
-	// 		return cell.input.value;
-	// 	} else {
-	// 		return 0;
-	// 	}
-	// });
+	/*const inputBoard = Array.from(gridElement.querySelectorAll(".cell"), cell => {
+		if (cell.input && cell.input.value) {
+			return cell.input.value;
+		} else {
+			return 0;
+		}
+	});*/
 
 	// one line version of the code above
 	const grid = Array.from(gridElement.querySelectorAll(".cell"), cell => (cell.firstChild && cell.firstChild.valueAsNumber) || 0);
@@ -88,7 +96,7 @@ solveButtonElement.addEventListener("click", async () => {
 	while (grid.length) {
 		inputBoard.push(grid.splice(0, parseInt(boardDimensionElement.value) + 2));
 	}
-	const requestJson = JSON.stringify({ inputBoard: inputBoard }); debugger;
+	
 	// get requested solutions number
 	const requestedSolutions = requestedSolutionsElement.value;
 
@@ -99,7 +107,6 @@ solveButtonElement.addEventListener("click", async () => {
 	// 	headers: { "Content-Type": "application/json" },
 	// });
 	// solutions = await response.json();
-	debugger;
 
 	// update solution count and current solution
 	solutionsFoundElement.innerHTML = solutions.length;
@@ -112,31 +119,68 @@ solveButtonElement.addEventListener("click", async () => {
 
 resetButtonElement.addEventListener("click", () => {
 
-	// TODO
-
-});
-
-previousButtonElement.addEventListener("click", () => {
-
-	currentSolutionIndex = currentSolutionIndex > 0 ? currentSolutionIndex - 1 : solutions.length - 1;
-	currentSolution.innerHTML = currentSolutionIndex + 1;
-	fillGrid(solutions[currentSolutionIndex]);
-
-});
-
-nextButtonElement.addEventListener("click", () => {
-
-	currentSolutionIndex = currentSolutionIndex < solutions.length - 1 ? currentSolutionIndex + 1 : 0;
-	currentSolution.innerHTML = currentSolutionIndex + 1;
-	fillGrid(solutions[currentSolutionIndex]);
-
-});
-
-// helper function to fill the grid
-function fillGrid(solution) {
-	solutions.forEach((value, index) => {
-		grid.querySelectorAll(".cell")[index].value = value;
+	resetSolutions();
+	gridElement.querySelectorAll(".cell input").forEach(input => {
+		input.value = 0;
 	});
+
+});
+
+exampleButtonElement.addEventListener("click", () => {
+
+	boardDimensionElement.value = 5;
+	changeGridDimension(7);
+	fillGrid(exampleValues);
+
+});
+
+function resetSolutions() {
+	
+	solutions = [];
+	solutionsFoundElement.innerHTML = 0;
+	currentSolutionElement.innerHTML = 0;
+
+}
+
+function changeGridDimension(dimension) {
+
+	let newGrid = "";
+
+	// create the new grid HTML
+	for (let i = 0; i < dimension; i++) {
+		for (let j = 0; j < dimension; j++) {
+
+			if ((i === 0 && j === 0) || (i === 0 && j === dimension - 1) || (i === dimension - 1 && j === 0) || (i === dimension - 1 && j === dimension - 1)) {
+				newGrid += cornerCellHTML;
+			} else if (i === 0 || i === dimension - 1 || j === 0 || j === dimension - 1) {
+				newGrid += frameCellHTML;
+			} else {
+				newGrid += boardCellHTML;
+			}
+			
+		}
+	}
+
+	// update the grid's innerHTML with the new grid
+	gridElement.innerHTML = newGrid;
+	// change the grid css for columns and rows, this will be inline, so it will have priority over the css file
+	gridElement.style["grid-template-columns"] = "repeat(" + dimension + ", 1fr)";
+	gridElement.style["grid-template-rows"] = "repeat(" + dimension + ", 1fr)";
+
+}
+
+function fillGrid(values) {
+
+	let cells = gridElement.querySelectorAll(".cell input");
+	let k = 0;
+
+	for (let i = 0; i < values.length; i++) {
+		for (let j = 0; j < values[i].length; j++) {
+			cells[k].value = values[i][j];
+			k++;
+		}
+	}
+
 }
 
 function escapeRegex(regex) {
